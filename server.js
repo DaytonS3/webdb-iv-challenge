5d1e6ac8df92c2b3c3434d518da4e78ff7c302c3
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-02 15:25:05 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// const dishRouter = require("./routers/dishRouter");
+// const recipeRouter = require("./routers/recipeRouter");
+
+const server = express();
+
+server.use(express.json());
+
+// server.use("/api/dish", dishRouter);
+// server.use("/api/recipe", recipeRouter);
+
+server.get("/", (req, res) => {
+  res.send("Server Running...");
+});
+
+module.exports = server;
